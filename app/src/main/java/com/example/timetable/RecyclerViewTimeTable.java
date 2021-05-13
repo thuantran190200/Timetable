f3cc714228ac8fc211ecf7d84e35e7d8a05983b0
Thuận Trần <thuantran190200@gmail.com>
2021-05-13 16:39:25 +0700

    add timetable and extends RecyclerView

---
+++ app/src/main/java/com/example/timetable/RecyclerViewTimeTable.java
@@ -0,0 +1,7 @@
+package com.example.timetable;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewTimeTable extends RecyclerView.Adapter<RecyclerViewTimeTable.ViewHolder>{
+
+}
